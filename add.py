2d8c1e541e14697e6e9e43a6e93c3145c36ab8af
Pau-Kin-Rui <164360875+Pau-Kin-Rui@users.noreply.github.com>
2024-08-23 09:21:30 +0800

    Update add.py 1

--- add.py
+++ add.py
@@ -1,14 +1,13 @@
 def sum(x, y):
-    ................
-    ................
+    answer = x+y
+    return answer
 
 def addition():
     a = int(input('Enter 1st number: '))
-    b = int(........('Enter 2nd number: '))
-    total = sum(..........)
+    b = int(input('Enter 2nd number: '))
+    total = sum(a, b)
     print(f'Sum of {a} and {b} is {total}')
 
 ## JANGAN UBAH KOD DI BAWAH ##
 if __name__ == "__main__":
     addition()
-
